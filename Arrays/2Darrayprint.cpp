26744f305ebe8c440910b27c8b83ece5a5c4e471
om-sarode-codes <om.sarode.codes@gmail.com>
2026-06-11 14:36:18 +0530

    2D arrays print

--- Arrays/2Darrayprint.cpp
+++ Arrays/2Darrayprint.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
-    int arr[4][3] = { {1, 2, 3},
-                      {4, 5, 6},
-                      {7, 8, 9},
-                      {10, 11, 12} };
+    int arr[4][3] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9},
+                     {10, 11, 12}};
 
-    int n=4;int m=3;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout<<arr[i][j]<<" ";
+    int n = 4;
+    int m = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
         }
-    }       
+    }
     return 0;
 }
