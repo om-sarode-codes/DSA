5749008596d083a2e37c0024d3685ab71341d7f1
om-sarode-codes <om.sarode.codes@gmail.com>
2026-06-11 14:54:25 +0530

    2D arrays print

--- Arrays/2Darrayprint.cpp
+++ Arrays/2Darrayprint.cpp
@@ -10,9 +10,11 @@ int main()
 
     int n = 4;
     int m = 3;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout<<arr[i][j]<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
         }
     }
     return 0;
