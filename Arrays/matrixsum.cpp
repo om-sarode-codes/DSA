ae53cee77758d4905a8d41b7db9f205a531588d1
om-sarode-codes <om.sarode.codes@gmail.com>
2026-06-12 12:56:40 +0530

    matrix sum

--- Arrays/matrixsum.cpp
+++ Arrays/matrixsum.cpp
@@ -1,31 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int arr[3][3];
-    int n=3;int m=3;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin>>arr[i][j];
+    int n = 3;
+    int m = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout<<"matrix : "<<arr[i][j]<<" ";;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << "matrix : " << arr[i][j] << " ";
+            ;
         }
     }
-    cout<<endl;
+    cout << endl;
 
-    int sum=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            sum+=arr[i][j];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            sum += arr[i][j];
         }
     }
-    cout<<"matrix sum : "<<sum;
+    cout << "matrix sum : " << sum;
 
-
-
-   
     return 0;
 }
