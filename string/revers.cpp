b88500f55a1cda174fd71e0f74298f0be3340741
om-sarode-codes <om.sarode.codes@gmail.com>
2026-06-13 13:34:27 +0530

    revers

---
+++ string/revers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    char ch[] = "helloaachal";
+
+    int st = 0;
+    int end = strlen(ch) - 1;
+
+    while (st <= end)
+    {
+        swap(ch[st], ch[end]);
+        st++;
+        end--;
+    }
+    cout << ch;
+
+    return 0;
+}
