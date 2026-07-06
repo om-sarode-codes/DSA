96743b72b9c243f1355ef196c48102f5bdc16df9
om-sarode-codes <om.sarode.codes@gmail.com>
2026-07-06 21:56:53 +0530

    lastoccurrence

--- recursion/lastoccurrence.cpp
+++ recursion/lastoccurrence.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int lastoccurrence(vector<int> arr,int n,int i,int target){
-    if(i==n){
+int lastoccurrence(vector<int> arr, int n, int i, int target)
+{
+    if (i == n)
+    {
         return -1;
     }
-    if(arr[i]==target){
+    if (arr[i] == target)
+    {
         return i;
     }
 
-    return lastoccurrence(arr,n,i-1,target);
-
+    return lastoccurrence(arr, n, i - 1, target);
 }
-int main(){
-    vector<int> arr={15,5,5,5,7,8,9};
-    cout<<lastoccurrence(arr,arr.size(),arr.size()-1,5);
+int main()
+{
+    vector<int> arr = {15, 5, 5, 5, 7, 8, 9};
+    cout << lastoccurrence(arr, arr.size(), arr.size() - 1, 5);
 
-  return 0;
+    return 0;
 }
