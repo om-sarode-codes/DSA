43095ed4137e52c772be9724d367c4444d151f71
om-sarode-codes <om.sarode.codes@gmail.com>
2026-07-09 13:54:01 +0530

    print odd  number..

---
+++ recursion/printodd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void print(int n, int i)
+{
+    if (i > n)
+    {
+        return;
+    }
+    if (i % 2 != 0)
+    {
+        cout << i << endl;
+    }
+    print(n, i + 1);
+}
+
+int main()
+{
+    int n = 10;
+    print(n, 1);
+
+    return 0;
+}
