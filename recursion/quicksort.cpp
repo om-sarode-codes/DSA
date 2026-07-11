d68cddad501c76a36b83688a21ba44ce03893338
om-sarode-codes <om.sarode.codes@gmail.com>
2026-07-11 22:03:08 +0530

    quick sort

---
+++ recursion/quicksort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int partistion(int arr[], int st, int end)
+{
+
+    int pivot = arr[end];
+    int i = st - 1;
+    for (int j = st; j < end; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    i++;
+    swap(arr[i], arr[end]);
+    return i;
+}
+
+void quicksort(int arr[], int st, int end)
+{
+    if (st >= end)
+    {
+        return;
+    }
+    int pivotidx = partistion(arr, st, end);
+
+    quicksort(arr, st, pivotidx - 1);
+    quicksort(arr, pivotidx + 1, end);
+}
+void printarr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 2, 8, 1, 7, 6, 3};
+    int n = 7;
+    quicksort(arr, 0, n - 1);
+    printarr(arr, n);
+}
